507e86d1e314d5cd1fea8e5de856c9c832182799
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-08 01:35:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,13 +20,8 @@ class App < Sinatra::Base
     end
     "#{full_phrase.strip}"
   end
-   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @one = params[:word1]
-    @two = params[:word2]
-    @three = params[:word3]
-    @four = params[:word4]
-    @five = params[:word5]
-    "#{@one} #{@two} #{@three} #{@four} #{@five}."
+   get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
    get '/:operation/:number1/:number2' do
     @operation = params[:operation]
