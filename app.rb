b82bfb04996b7c171152998f30c4ddcdb664baf9
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-08 01:36:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,11 +6,12 @@ class App < Sinatra::Base
     @name = params[:name]
     "#{@name.reverse}"
   end
- 
+
   get '/square/:number' do
     @num = params[:number]
     "#{@num.to_i * @num.to_i}"
   end
+  
    get '/say/:number/:phrase' do
     @phrase = params[:phrase]
     @num = params[:number].to_i
@@ -20,9 +21,11 @@ class App < Sinatra::Base
     end
     "#{full_phrase.strip}"
   end
+  
    get '/say/:word1/:word2/:word3/:word4/:word5' do 
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
+  
    get '/:operation/:number1/:number2' do
     @operation = params[:operation]
     @one = params[:number1].to_i
