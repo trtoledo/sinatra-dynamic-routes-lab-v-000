0edbcaed36520eeb708809e324667b5b25f21712
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-08 01:41:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,9 +10,8 @@ class App < Sinatra::Base
     "#{params[:number].to_i * params[:number].to_i}"
   end
 
-   get '/say/:number/:phrase' do
-    @phrases = params[:phrase] * params[:number].to_i
-    "#{@phrases}"
+   get '/say/:number/:phrase' do 
+    "#{params[:phrase] * params[:number].to_i}"
    end
 
    get '/say/:word1/:word2/:word3/:word4/:word5' do
